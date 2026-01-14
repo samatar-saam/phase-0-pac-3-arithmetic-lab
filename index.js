b24ab095f92310a7b6c1e369ef748a5ef9e67a59
samatar mohamed <samatarmohamed59@gmail.com>
2026-01-14 10:14:04 +0300

    done

---
+++ index.js
@@ -0,0 +1,35 @@
+// Basic math functions
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return n + 1;
+}
+
+function decrement(n) {
+  return n - 1;
+}
+
+// Parsing functions
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
